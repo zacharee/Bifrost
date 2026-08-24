51357cef190ff019191d7847be79321c6889c326
Zachary Wander <zachary.wander@gmail.com>
2026-08-24 06:55:50 -0400

    Include custom Ketch libs

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,7 +21,6 @@ dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         mavenCentral()
-        mavenLocal()
         google()
 
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
